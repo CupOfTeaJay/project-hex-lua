2d9a1e0f37f6336a33c1a0292a4bad9b02b6659d
Thomas Given <tjgiven17@protonmail.com>
2025-06-11 21:35:28 -0400

    more wfc slop

--- mapgen/wfc.lua
+++ mapgen/wfc.lua
@@ -6,6 +6,7 @@
 --]]
 
 local bms = require("utils.bms")
+local dict = require("utils.containers").dict
 local map = require("mapgen.map")
 
 local wfc = {}
@@ -15,84 +16,89 @@ local wfc = {}
 ---
 wfc.WaveFunction = {
     new = function(states)
+        local wave_function = {dict.new({})}
+        for _, state in pairs(states) do
+            wave_function[1]:insert(state, 1.0/#states)
+        end
+        setmetatable(wave_function, wfc.WaveFunction.metatable)
+        return wave_function
     end,
     methods = {
+        collapse = function(self)
+            local rand = math.random()
+            local cursor = 0
+            local choice = nil
+            for key, val in pairs(self[1]:iter()) do
+                cursor = cursor + val
+                if cursor > rand then
+                    choice = key
+                    break
+                end
+            end
+            self[1]:clear()
+            return choice
+        end,
+        get_entropy = function(self)
+            return self[1].size
+        end,
+is_collapsed = function(self)
+            return self[1].size == 0
+        end,
+        propagate = function(self, choice, sockets)
+            for state, prob in pairs(self[1]:iter()) do
+                if sockets[choice] and not sockets[choice]:contains(state) then
+                    self[1]:remove(state)
+                end
+            end
+        end,
     },
 }
 wfc.WaveFunction.metatable = {
     __index = wfc.WaveFunction.methods,
 }
 
-function wfc._get_lowest_entropy(nodes)
+function wfc._get_lowest_entropy(template)
     local min = nil
-    local row = nil
-    local col = nil
-    local ref = nil
-    for _row, _col, _ref in nodes:iter() do
-        if not _ref.collapsed and (min == nil or _ref.entropy < min) then
-            row = _row
-            col = _col
-            ref = _ref
-            min = _ref.entropy
+    local _row = nil
+    local _col = nil
+    local _ref = nil
+    for row, col, ref in template:iter() do
+        if not ref.wave_function:is_collapsed() then
+            local entropy = ref.wave_function:get_entropy()
+            if min == nil or entropy < min then
+                min = entropy
+                _row = row
+                _col = col
+                _ref = ref
+            end
         end
     end
-    return row, col, ref
+    return _row, _col, _ref
 end
 
 ---
 --- Entry-point for Wave Function Collapse.
 ---
 function wfc.collapse(template, sockets)
-    -- Map template for WFC nodes.
-    local nodes = map.template.new(#template[1], #template)
-
-    -- Determine tile possibilities from sockets.
-    local choices = {}
-    for choice, _ in pairs(sockets) do
-        table.insert(choices, choice)
-    end
-
-    -- Initialize nodes. We want to update the 'Debug' tiles here. Also
-    -- determine possible nucleation sites.
-    local nucleation_sites = {}
-    for row, col, ref in template:iter() do
-        local tile = bms.getc(ref.entity, "Tile")
-        if tile.biome == "Debug" then
-            nodes[row][col].choices = choices
-            nodes[row][col].entropy = #choices
-            nodes[row][col].collapsed = false
-            table.insert(nucleation_sites, {row = row, col = col})
-        else
-            nodes[row][col].collapsed = true
+    local row, col, ref = wfc._get_lowest_entropy(template)
+    while ref do
+        local choice = ref.wave_function:collapse()
+        bms.insc(
+            ref.entity,
+            "Tile",
+            bms.new(
+                "Tile",
+                {
+                    biome = choice,
+                    terrains = {},
+                    features = {}
+                }
+            )
+        )
+        for _, neighbor in pairs(template:get_neighbors(row, col)) do
+            neighbor.wave_function:propagate(choice, sockets)
         end
-    end
-
-    -- Begin the Wave Function Collapse algorithm.
-    local site = nucleation_sites[math.random(#nucleation_sites)]
-    local node = nodes[site.row][site.col]
-    while node do
-        -- Collapse.
-        local choice = node.choices[math.random(node.entropy)]
-        local tile = bms.getc(template[site.row][site.col].entity, "Tile")
-        tile.biome = choice
-        nodes[site.row][site.col].collapsed = true
-
-        -- Propagate.
-        for _, neighbor in pairs(nodes:get_neighbors(site.row, site.col)) do
-            if not neighbor.collapsed then
-                local new_choices = {}
-                for _, possibility in pairs(neighbor.choices) do
-                    if sockets[possibility]:contains(choice) then
-                        table.insert(new_choices, possibility)
-                    end
-                end
-                neighbor.choices = new_choices
-                neighbor.entropy = #neighbor.choices
-            end
-        end
-
-        -- Get next node.
-        site.row, site.col, node = wfc._get_lowest_entropy(nodes)
+        row, col, ref = wfc._get_lowest_entropy(template)
     end
 end
 
--- utils/containers.lua
+++ utils/containers.lua
@@ -7,6 +7,57 @@
 
 local containers = {}
 
+containers.dict = {
+    new = function(t)
+        local dict = {size = 0}
+        for key, val in pairs(t) do
+            dict[key] = val
+            dict.size = dict.size + 1
+        end
+        setmetatable(dict, containers.dict.metatable)
+        return dict
+    end,
+    methods = {
+        clear = function(self)
+            for key, _ in pairs(self) do
+                if key ~= "size" then
+                    self[key] = nil
+                    self.size = self.size - 1
+                end
+            end
+        end,
+        contains = function(self, key)
+            if self[key] then return true else return false end
+        end,
+        insert = function(self, key, val)
+            if not self[key] then
+                self[key] = val
+                self.size = self.size + 1
+            else
+                self[key] = val
+            end
+        end,
+        iter = function(self)
+            local items = {}
+            for key, val in pairs(self) do
+                if key ~= "size" then
+                    items[key] = val
+                end
+            end
+            return items
+        end,
+        remove = function(self, key)
+            if self[key] then
+                self[key] = nil
+                self.size = self.size - 1
+            end
+        end
+    },
+}
+containers.dict.metatable = {
+    __index = containers.dict.methods
+}
+
 containers.set = {
     new = function(arr)
         local set = {size = 0}
